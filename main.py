893e158795fe4fe14fda52baf28055839b6fb210
Nikita090990 <byilovnikita@gmail.com>
2024-10-08 21:53:17 +0300

    add get_set

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ class Todo:
     def __init__(self):
         self.issue = {}
         self.count = 0
+        self.__count_no_complete = 0
+        self.__count_transfer = 0
 
-    def add_issue(self, issue, clock, complite):
-        self.issue[issue] = [clock, complite]
+    def add_issue(self, issue, clock, complite, transfer):
+        self.issue[issue] = [clock, complite, transfer]
         self.count += 1
 
     def delete_issue(self, issue):
@@ -13,22 +15,45 @@ class Todo:
     
     def get_count(self):
         return self.count
+    @property
+    def count_no_complete(self):
+        return self.__count_no_complete
+    
+    @property
+    def count_transfer(self):
+        return self.__count_transfer
+    
+    @count_no_complete.setter
+    def count_no_complete(self, count_no_complete):
+        self.__count_no_complete = count_no_complete
+
+    @count_transfer.setter
+    def set_count_transfer(self, count_transfer):
+        self.__count_transfer = count_transfer
     
     def change_issue(self, issue):
         temp = self.issue.get(issue)
         temp[1] = True
         self.issue.update({issue:temp})
+    
+    def change_transfer(self, issue):
+        temp = self.issue.get(issue)
+        temp[2] = True
+        self.issue.update({issue:temp})
 
     def show(self):
         nums = 1
         for key,value in self.issue.items():
-            print(f'{nums}.{key} time:{value[0]} {"Выполнено"if value[1] else "Не выполнено"} ')
+            print(f'{nums}.{key} time:{value[0]} {"Выполнено"if value[1] else "Не выполнено"} {"Перенесено"if value[2] else "Не перенесено"} ')
             nums += 1
 
 todo = Todo()
-todo.add_issue('Завтрак', '8:30', True)
-todo.add_issue('Уборка', '10:00', False)
-todo.add_issue('Кормление кота', '12:30', False)
+todo.add_issue('Завтрак', '8:30', True, False)
+todo.add_issue('Уборка', '10:00', False, False)
+todo.add_issue('Кормление кота', '12:30', False, False)
 todo.show()
 todo.change_issue('Уборка')   
 todo.show()
+
+todo.count_no_complete = 5
+print(todo.count_no_complete)
